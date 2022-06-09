8da496c1db85556a5abf8d84a705deed13927946
EugenioSirito <eugenio_sirito@hotmail.com>
2022-06-09 15:13:24 -0300

    add sigup layout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,109 @@
-import React from 'react';
-import i18next from 'i18next';
+import React, { useState } from 'react';
 
-import logo from './assets/logo.svg';
+import wolox from './assets/wolox.png';
 import styles from './styles.module.scss';
 
 function Home() {
+  type Usuario = {
+    nombre: string;
+    apellido: string;
+    email: string;
+    password: string;
+    confPass: string;
+  };
+  const defaultFormData: Usuario = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    password: '',
+    confPass: ''
+  };
+
+  const [formData, setFormData] = useState(defaultFormData);
+  const { nombre, apellido, email, password, confPass } = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }));
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = { nombre, apellido, email, password, confPass };
+    console.log(user);
+    setFormData(defaultFormData);
+  };
+
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt={i18next.t('Home:logoAlt') as string} />
-        <p className={styles.text}>{i18next.t('Home:welcome')}</p>
-      </header>
+      <form onSubmit={onSubmit}>
+        <img src={wolox} alt="" />
+
+        <div className={styles.field}>
+          <label htmlFor="nombre" className={styles.nombre}>
+            Nombre
+          </label>
+          <br />
+          <input type="text" id="nombre" className={styles.input} value={nombre} onChange={onChange} />
+          <br />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="apellido" className={styles.apellido}>
+            Apellido
+          </label>
+          <br />
+          <input type="text" id="apellido" className={styles.input} value={apellido} onChange={onChange} />
+          <br />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="email" className={styles.email}>
+            Email
+          </label>
+          <br />
+          <input type="mail" id="email" className={styles.input} value={email} onChange={onChange} />
+          <br />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="password" className={styles.password}>
+            Password
+          </label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            className={styles.input}
+            value={password}
+            onChange={onChange}
+          />
+          <br />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="confPass" className={styles.confPass}>
+            Confirmación de Password
+          </label>
+          <br />
+          <input
+            type="password"
+            id="confPass"
+            className={styles.input}
+            value={confPass}
+            onChange={onChange}
+          />
+        </div>
+        <br />
+        <button type="submit" className={styles.sign}>
+          Sign Up
+        </button>
+        <hr />
+        <button type="submit" className={styles.log}>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
