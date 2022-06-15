c145aaf7438dac40d85d99af4bc0e1f2023bbbfc
EugenioSirito <eugenio_sirito@hotmail.com>
2022-06-15 12:15:40 -0300

    Router added to the proyect

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import 'scss/application.scss';
 import Home from 'screens/Home/index';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/sign_up">
+          <Home />
+        </Route>
+        <Route path="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
