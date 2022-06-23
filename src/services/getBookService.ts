28fc66f4765c4b09d0cf36b63fddfa9352f6fd6b
EugenioSirito <eugenio_sirito@hotmail.com>
2022-06-23 13:26:10 -0300

    Fixes

--- src/services/getBookService.ts
+++ src/services/getBookService.ts
@@ -2,9 +2,6 @@ import api from 'config/api';
 
 export const getBooks = (headers: { token: string | null; uid: string | null; client: string | null }) => {
   const { token, uid, client } = headers;
-  console.log(token);
-  console.log(uid);
-  console.log(client);
   return api.get(
     '/books',
     {},
