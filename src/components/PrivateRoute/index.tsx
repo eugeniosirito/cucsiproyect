b22ba6e0d556719c6a4ae25658a2af53d7928cd2
EugenioSirito <eugenio_sirito@hotmail.com>
2022-06-23 15:19:58 -0300

    added constant values

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 
+import { STORAGE_VALUES } from 'constants/constantsStorageValues';
 import LocalStorageService from 'services/LocalStorageService';
 
 function PrivateRoute() {
-  const auth = LocalStorageService.getValue('logged');
+  const auth = LocalStorageService.getValue(STORAGE_VALUES.tokenValue);
   return auth ? <Outlet /> : <Navigate to="/" replace />;
 }
 
