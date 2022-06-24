2fcfed31ecf2253cd603ba2e9d4d7a17157b50ce
EugenioSirito <eugenio_sirito@hotmail.com>
2022-06-24 14:02:35 -0300

    added constant route

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -3,10 +3,11 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 import { STORAGE_VALUES } from 'constants/constantsStorageValues';
 import LocalStorageService from 'services/LocalStorageService';
+import { PATH_NAMES } from 'constants/constantsPaths';
 
 function PrivateRoute() {
   const auth = LocalStorageService.getValue(STORAGE_VALUES.tokenValue);
-  return auth ? <Outlet /> : <Navigate to="/" replace />;
+  return auth ? <Outlet /> : <Navigate to={PATH_NAMES.login} replace />;
 }
 
 export default PrivateRoute;
